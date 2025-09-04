78f6c618f5f22c0d976a8b572c00ea1e799b5d3d
mayodi3 <mayodisaac3@gmail.com>
2025-09-04 10:45:52 +0300

    Managing Environment Variables

---
+++ packages/server/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Isaac");
+});
+
+app.listen(port, () => {
+  console.log(`Server Listening on http://localhost:${port}`);
+});
