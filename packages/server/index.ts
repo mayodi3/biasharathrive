50d768f82815aa168f8072fecb0814340c10fe1d
mayodi3 <mayodisaac3@gmail.com>
2025-09-04 11:05:04 +0300

    Run both apps together

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
   res.send("Hello Isaac");
